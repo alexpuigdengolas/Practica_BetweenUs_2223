6ce715342fda55fa502609a93575193a54447d8d
arnau <arnau.serra@students.salle.url.edu>
2023-04-21 13:11:42 +0200

    New Game view i anar desde start a new game.

--- src/presentation/views/NewGameView.java
+++ src/presentation/views/NewGameView.java
@@ -7,20 +7,20 @@ import java.awt.event.ActionListener;
 public class NewGameView extends JPanel {
 
 
-    public static final String BTN_P = "BTN_P";
+    public static final String BTN_CHA = "BTN_CHA";
     public static final String BTN_BACK = "BTN_BACK";
     public static final String BTN_STI = "BTN_STI";
 
-    private JButton jbProva = new JButton("Prova");
+    private JTextField nameField = new JTextField();
+
+
+    private JButton chargeButton = new JButton("Charge");
     private JButton jbBack = new JButton();
     private JButton jbSettings = new JButton();
 
-    private JTextField nameSpace = new JTextField();
-
-
-    private MainView mainView;
 
     private CardLayout components;
+    private MainView mainView;
 
     public NewGameView(){
         configureNGView();
@@ -40,11 +40,12 @@ public class NewGameView extends JPanel {
         this.add(jpActions, BorderLayout.NORTH);
 
         JPanel titlePanel = new JPanel();
-        JLabel jlTitle = new JLabel("NEW GAME");
+        JLabel jlTitle = new JLabel("Charge game");
         jlTitle.setHorizontalAlignment((int) CENTER_ALIGNMENT);
-        jlTitle.setFont(new Font("Arial", Font.PLAIN, 40));
+        jlTitle.setFont(new Font("Serif", Font.PLAIN, 40));
         titlePanel.add(jlTitle);
         //this.add(titlePanel, BorderLayout.NORTH);
+
 
         JPanel voidPanel = new JPanel();
         voidPanel.setPreferredSize(new Dimension(50, 50));
@@ -53,31 +54,63 @@ public class NewGameView extends JPanel {
 
         JPanel infoPanel = new JPanel();
         infoPanel.setLayout(new BoxLayout(infoPanel, BoxLayout.Y_AXIS));
-        //Label de Login
-        JLabel jlGame = new JLabel("GAME NAME");
-        jlGame.setHorizontalAlignment((int) CENTER_ALIGNMENT);
-        jlGame.setFont(new Font("Arial", Font.PLAIN, 20));
+        //Label de cargar partida
+        JLabel jlCharge = new JLabel("Game name");
+        jlCharge.setHorizontalAlignment((int) CENTER_ALIGNMENT);
+        jlCharge.setFont(new Font("Serif", Font.PLAIN, 20));
         //TODO: Cambiar el color de la vista
-        infoPanel.add(jlGame);
+        infoPanel.add(jlCharge);
 
-        //TextField de New Game
-        nameSpace.setMaximumSize(new Dimension(500, nameSpace.getPreferredSize().height));
+
+
+        //TextField de cargar partida
+        nameField.setMaximumSize(new Dimension(500, nameField.getPreferredSize().height));
         infoPanel.add(Box.createHorizontalGlue()); // Add horizontal glue to align the text field to the center
-        infoPanel.add(nameSpace);
-        //infoPanel.add(Box.createVerticalStrut(10)); // Add vertical space
+        infoPanel.add(nameField);
+        infoPanel.add(Box.createVerticalStrut(10));
+        this.add(infoPanel);
+        //Panel de Jlabels
+        //jlabel 1
+        JPanel labelPanel = new JPanel();
+        JLabel label = new JLabel("Color");
+        JTextField textField = new JTextField();
+        textField.setColumns(20); // Establecer 20 caracteres de ancho
+        labelPanel.add(label);
+        labelPanel.add(textField);
+        infoPanel.add(labelPanel);
 
 
-        JPanel actionPanel= new JPanel();
 
-        jbProva.setActionCommand(BTN_P);
-        actionPanel.add(jbProva);
 
-        this.add(actionPanel, BorderLayout.SOUTH);
+        //jlabel 2
+        JLabel label2 = new JLabel("Impostors");
+        JTextField textField2 = new JTextField();
+        textField.setColumns(20); // Establecer 20 caracteres de ancho
+        labelPanel.add(label2);
+        labelPanel.add(textField2);
+        infoPanel.add(labelPanel);
+        //jlabel 3
+        JLabel label3 = new JLabel("Players");
+        labelPanel.add(label3);
+        infoPanel.add(label3);
+        //jlabel 3
+        JLabel label4 = new JLabel("Map");
+        labelPanel.add(label4);
+        infoPanel.add(label4);
+
+        //Panel de botones
+        JPanel buttonPanel = new JPanel();
+        chargeButton.setActionCommand(BTN_CHA);
+        buttonPanel.add(chargeButton);
+
+        infoPanel.add(buttonPanel, BorderLayout.SOUTH);
+
     }
 
     public void NGController(ActionListener actionListener){
         jbBack.addActionListener(actionListener);
-        jbProva.addActionListener(actionListener);
+        chargeButton.addActionListener(actionListener);
+        jbSettings.addActionListener(actionListener);
     }
 
 
