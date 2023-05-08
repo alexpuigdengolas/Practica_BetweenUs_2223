ff77dff73d310811ba4c519016392afab4346378
Mario-ra15 <mario.ra@students.salle.url.edu>
2023-05-08 12:07:32 +0200

    Borrado comentarios y codigo no necesario

--- src/business/entities/map/Mobility.java
+++ src/business/entities/map/Mobility.java
@@ -4,7 +4,7 @@ import com.google.gson.annotations.Expose;
 import com.google.gson.annotations.SerializedName;
 
 public class Mobility {
-    // Attributes
+
     @SerializedName("left")
     @Expose
     private Integer left;
