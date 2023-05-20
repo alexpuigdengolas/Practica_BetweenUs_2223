2443bcd67629fef9e7dcd88096259596db3fb579
arnau <arnau.serra@students.salle.url.edu>
2023-05-20 19:08:46 +0200

    Es carrega el user al mapa y ens podem moure pel mapa

--- src/presentation/views/custom/StatisticPanel.java
+++ src/presentation/views/custom/StatisticPanel.java
@@ -5,11 +5,10 @@ import java.awt.*;
 import java.util.ArrayList;
 
 public class StatisticPanel extends JPanel {
-
     private static final int CHART_WIDTH = 400;
     private static final int CHART_HEIGHT = 300;
     private static final int MARGIN = 40;
-    private static final int TICK_LENGTH = 10;
+    private static final int TICK_LENGTH = 15;
     private static final int TICK_DISTANCE = 10;
     private float[] data;
 
@@ -23,58 +22,79 @@ public class StatisticPanel extends JPanel {
         super.paintComponent(g);
         Graphics2D g2d = (Graphics2D) g;
 
-        int minValue = 0;
-        int maxValue = 100;
+        float minValue = Float.MAX_VALUE;
+        float maxValue = Float.MIN_VALUE;
+        for (float value : data) {
+            if (value < minValue) {
+                minValue = value;
+            }
+            if (value > maxValue) {
+                maxValue = value;
+            }
+        }
+
+        // Ajustar los valores mínimo y máximo al rango deseado
+        minValue = Math.min(minValue, 0);
+        maxValue = Math.max(maxValue, 100);
 
         int xUnit = (CHART_WIDTH - 2 * MARGIN) / (data.length - 1);
-        int yUnit = (CHART_HEIGHT - 2 * MARGIN) / (maxValue - minValue);
+        int yUnit = (CHART_HEIGHT - 2 * MARGIN) / (int) (maxValue - minValue);
 
         g2d.setColor(Color.BLACK);
 
-        // Draw x-axis
-        g2d.drawLine(MARGIN, CHART_HEIGHT - MARGIN, CHART_WIDTH - MARGIN, CHART_HEIGHT - MARGIN);
+        int chartCenterX = getWidth() / 2;
+        int chartCenterY = getHeight() / 2;
 
-        // Draw y-axis
-        g2d.drawLine(MARGIN, CHART_HEIGHT - MARGIN, MARGIN, MARGIN);
+        int chartStartX = chartCenterX - CHART_WIDTH / 2;
+        int chartStartY = chartCenterY - CHART_HEIGHT / 2;
 
-        // Draw x-axis tick marks and labels
+        String title = "Stadistics";
+        FontMetrics fontMetrics = g2d.getFontMetrics();
+        int titleWidth = fontMetrics.stringWidth(title);
+        int titleX = chartCenterX - titleWidth / 2;
+        int titleY = chartStartY - MARGIN-100;
+        g2d.drawString(title, titleX, titleY);
+
+
+
+        g2d.drawLine(chartStartX, chartCenterY, chartStartX + CHART_WIDTH, chartCenterY);
+
+        g2d.drawLine(chartStartX, chartCenterY, chartStartX, chartStartY);
+
         int xTickSpacing = (CHART_WIDTH - 2 * MARGIN) / (data.length - 1);
         for (int i = 0; i < data.length; i++) {
-            int x = MARGIN + i * xTickSpacing;
-            int y = CHART_HEIGHT - MARGIN;
+            int x = chartStartX + i * xTickSpacing;
+            int y = chartCenterY;
             g2d.drawLine(x, y, x, y + TICK_LENGTH);
-            g2d.drawString(String.valueOf(i), x - 5, y + TICK_LENGTH + 15);
+            g2d.drawString(String.valueOf(i), x - 5, y + TICK_LENGTH +15);
         }
 
-        // Draw y-axis tick marks and labels
-        int yTickSpacing = (CHART_HEIGHT - 2 * MARGIN) / (maxValue - minValue);
-        for (int i = minValue; i <= maxValue; i += TICK_DISTANCE) {
-            int x = MARGIN;
-            int y = CHART_HEIGHT - MARGIN - (i - minValue) * yTickSpacing;
-            g2d.drawLine(x, y, x - TICK_LENGTH, y);
+        int yTickSpacing = (CHART_HEIGHT - 2 * MARGIN) / (int) (maxValue - minValue);
+        for (int i = (int) minValue; i <= maxValue; i += TICK_DISTANCE) {
+            int x = chartStartX;
+            int y = chartCenterY - (i - (int) minValue) * yTickSpacing;
+            g2d.drawLine(x, y, x - (TICK_LENGTH), y);
             g2d.drawString(String.valueOf(i), x - TICK_LENGTH - 30, y + 5);
         }
 
         g2d.setColor(Color.RED);
 
-        // Draw data points and lines
         for (int i = 0; i < data.length; i++) {
-            int x = MARGIN + i * xUnit;
-            float y = CHART_HEIGHT - MARGIN - (data[i] - minValue) * yUnit;
+            int x = chartStartX + i * xUnit;
+            float y = chartCenterY - (data[i] - minValue) * yUnit;
 
-            g2d.fillOval(x - 2, (int) (y - 2), 4, 4);
+            g2d.fillOval(x - 2, (int) y - 2, 4, 4);
 
-            if (i < data.length - 1) {
-                int x2 = MARGIN + (i + 1) * xUnit;
-                float y2 = CHART_HEIGHT - MARGIN - (data[i + 1] - minValue) * yUnit;
-                g2d.drawLine(x, (int)y, x2, (int)y2);
+            if (i > 0) {
+                int xPrev = chartStartX + (i - 1) * xUnit;
+                float yPrev = chartCenterY - (data[i - 1] - minValue) * yUnit;
+                g2d.drawLine(xPrev, (int) yPrev, x, (int) y);
             }
         }
     }
 
-    public void updateData(float[] newData) {
-        data = newData;
-        repaint();
-    }
 
+    public void updateData(float[] newData) {
+        this.data = newData;
+    }
 }
